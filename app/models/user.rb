d03cd1874a5eb7aa45e1b90295f88c92997f9553
jayasi <jayasi05@gmail.com>
2015-07-16 13:37:52 +0530

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
 
+	validates_presence_of :email, :first_name, :last_name, :country, :address
+	# Till the time we are not using a proper authentication method 
+  attr_accessor :password, :password_confirmation	
+
   has_many :address, :dependent => :destroy
   has_many :order, :dependent => :destroy
 

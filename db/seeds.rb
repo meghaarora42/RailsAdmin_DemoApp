40af478b4e89de4fa552cf5b259ba4b1a8b62c49
Megha <megha893.arora@gmail.com>
2015-07-22 00:41:32 +0530

    Fixed user and address model to seed data correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@ require 'active_record'
     country: country,
     first_name: FFaker::Name.first_name,
     last_name: FFaker::Name.last_name,
-    address: Address.create!(
+    addresses: [Address.create!(
 	  house_no: FFaker::Address.secondary_address,
 	  street_name: FFaker::Address.street_address,
 	  city: FFaker::Address.city, 
       country: country,
 	  pin_code: 1 + rand(99999)
-	)
+	)]
   ) 
 end 
